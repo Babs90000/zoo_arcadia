299e29ed5c7487b46c406b6f5538315a3879e29d
Babs90000 <b.camara.diaby@outlook.com>
2025-01-12 02:33:05 +0100

    dernierè sauvegarde avant fusion

--- js/script.js
+++ js/script.js
@@ -1,79 +1,75 @@
 async function animalSearch() {
-	let requete = document.getElementById("animalSearchBar").value;
+  let requete = document.getElementById("animalSearchBar").value;
 
-	let url;
+  let url;
 
-	if (requete.length === 0) {
-		url = "../pages/recherche_animal.php";
-	} else {
-		url = `../pages/recherche_animal.php?q=${encodeURIComponent(requete)}`;
-	}
+  if (requete.length === 0) {
+    url = "../pages/recherche_animal.php";
+  } else {
+    url = `../pages/recherche_animal.php?q=${encodeURIComponent(requete)}`;
+  }
 
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Erreur réseau");
+    }
 
-	try {
-		let response = await fetch(url);
-		if (!response.ok) {
-			throw new Error("Erreur réseau");
-		}
+    let data = await response.json();
+    console.log("Réponse JSON du serveur :", data);
 
-		let data = await response.json(); 
-		console.log("Réponse JSON du serveur :", data);
+    let resultatSearch = document.getElementById("resultatSearch");
+    resultatSearch.innerHTML = "";
 
-		let resultatSearch = document.getElementById("resultatSearch");
-		resultatSearch.innerHTML = ""; 
+    if (data.length > 0) {
+      data.forEach((animal) => {
+        let pName = document.createElement("p");
+        let pAge = document.createElement("p");
+        let pRace = document.createElement("p");
+        let img = document.createElement("img");
+        let link = document.createElement("a");
+        let animalDiv = document.createElement("div");
 
-		if (data.length > 0) {
-			data.forEach((animal) => {
-				let pName = document.createElement("p");
-				let pAge = document.createElement("p");
-				let pRace = document.createElement("p");
-				let img = document.createElement("img");
-				let link = document.createElement("a");
-				let animalDiv = document.createElement("div");
+        pName.textContent = `Prénom: ${animal.prenom}`;
+        pAge.textContent = `Âge: ${animal.age} ans`;
+        pRace.textContent = `Race: ${animal.label}`;
+        img.src = `data:image/jpeg;base64,${animal.image_data}`;
+        link.href = `detail_animal.php?animal_id=${animal.animal_id}`;
+        link.textContent = "Voir la fiche de l'animal";
 
-				pName.textContent = `Prénom: ${animal.prenom}`;
-				pAge.textContent = `Âge: ${animal.age} ans`;
-				pRace.textContent = `Race: ${animal.label}`;
-				img.src = `data:image/jpeg;base64,${animal.image_data}`;
-				link.href = `detail_animal.php?animal_id=${animal.animal_id}`;
-				link.textContent = "Voir la fiche de l'animal";
+        pName.classList.add("animalName");
+        pAge.classList.add("animalAge");
+        pRace.classList.add("animalRace");
+        img.classList.add("animalImage");
+        link.classList.add("animalLink");
+        animalDiv.classList.add("animal");
 
-				pName.classList.add("animalName");
-				pAge.classList.add("animalAge");	
-				pRace.classList.add("animalRace");
-				img.classList.add("animalImage");
-				link.classList.add("animalLink");
-				animalDiv.classList.add("animal");
+        animalDiv.appendChild(pName);
+        animalDiv.appendChild(pAge);
+        animalDiv.appendChild(pRace);
+        animalDiv.appendChild(img);
+        animalDiv.appendChild(link);
 
-
-				animalDiv.appendChild(pName);
-				animalDiv.appendChild(pAge);
-				animalDiv.appendChild(pRace);
-				animalDiv.appendChild(img);
-				animalDiv.appendChild(link);
-
-				
-				resultatSearch.appendChild(animalDiv);
-			});
-		} else {
-			resultatSearch.textContent = "Aucun résultat trouvé.";
-		}
-	} catch (error) {
-		console.error("Error:", error);
-		
-	}
+        resultatSearch.appendChild(animalDiv);
+      });
+    } else {
+      resultatSearch.textContent = "Aucun résultat trouvé.";
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", animalSearch);
 
-
 // affichage du menu mobile
-document.getElementById("button_menu_mobile").addEventListener("click", function () {
-		const navbarMobile = document.getElementById("navbar_mobile");
-		navbarMobile.classList.toggle("show");
-	});
+document
+  .getElementById("button_menu_mobile")
+  .addEventListener("click", function () {
+    const navbarMobile = document.getElementById("navbar_mobile");
+    navbarMobile.classList.toggle("show");
+  });
 
-
-        function goBack() {
-            window.history.back();
-        }
+function goBack() {
+  window.history.back();
+}
